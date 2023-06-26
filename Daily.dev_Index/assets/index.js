507e7f58463723cef6926e2e2bebd43df26e50a6
thucDuong <103923238+Akatsuki-Naruto@users.noreply.github.com>
2023-06-26 18:15:25 +0700

    build: complete call API and render to the scree

--- Daily.dev_Index/assets/index.js
+++ Daily.dev_Index/assets/index.js
@@ -1,14 +1,75 @@
-const fakeAPI = async () => {
-    const response = await fetch()
+// var Tag = 'http://localhost:3000/Tag';
+
+// fetch(Tag)
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function(tags) {
+//         console.log(tags)
+//     })
+
+
+var tagAPI = 'http://localhost:3000/Tag'
+
+function start() {
+    getTags(function (Tag) {
+        renderTags(Tag)
+    });
 }
 
-(function (){
-    var els = [ 'section', 'article', 'hgroup', 'header', 'footer', 'nav', 'aside', 
-    'figure', 'mark', 'time', 'ruby', 'rt', 'rp' ];
-    for (var i=0; i<els.length; i++){
-        document.createElement(els[i]);
-        }
-})();
+start();
 
+// Functions
+
+function getTags(callback) {
+    fetch(tagAPI)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(callback);
+}
+
+function renderTags(Tag) {
+    var listTagBlock = document.querySelector('#listTags');
+    var htmls = Tag.map(function(Tag) {
+        return `
+        <button class="tag${Tag.id} buttonTag">
+            <img class="avatarTag" src="${Tag.avatar}" alt="">
+            <div class="titleTag">${Tag.title}</div>
+            <div class="timeTag"></div>
+            <img class="imageTag" src="${Tag.picture}" alt="">
+        </button>`;
+        
+    });
+    listTagBlock.innerHTML = htmls.join('');
+}
+
+
+
+
+
+
+
+
+
+// fetch(tagAPI) 
+//     .then(response => response.json())
+//     .then(function(Tags){
+//         var htmls = Tags.map(function (Tag) {
+//             return `
+//             <button >
+//                 <div class="avatarTag"></div>
+//                 <div class="tittleTag">${Tag.title}</div>
+//                 <div class="timeTag"></div>
+//                 <div class="imageTag">${Tag.picture}</div>
+//             </button>`;
+//         });
+
+//         var html = htmls.join('');
+//         document.getElementById('listTags').innerHTML = html
+//     })
+//     .catch(function(err){
+//         alert('error')
+// })
 
 
---
+++ Readme.md
@@ -0,0 +1,7 @@
+How to activate:
+    - Use json-server create localhost: http://localhost:3000/Tag
+    - Javascript call API by fetch (method: GET)
+    - Method GET give a request data to Json-server
+    - server return a json-file
+    - Method GET analysis to html output
+    - Javascript: render to html output 
