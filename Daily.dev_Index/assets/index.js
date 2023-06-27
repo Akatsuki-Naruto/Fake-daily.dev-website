b943d611a95e362850f5d7643c79fad9990d649d
thucDuong <103923238+Akatsuki-Naruto@users.noreply.github.com>
2023-06-27 11:35:00 +0700

    build: update link Api

--- Daily.dev_Index/assets/index.js
+++ Daily.dev_Index/assets/index.js
@@ -1,15 +1,5 @@
-// var Tag = 'http://localhost:3000/Tag';
 
-// fetch(Tag)
-//     .then(function (response) {
-//         return response.json();
-//     })
-//     .then(function(tags) {
-//         console.log(tags)
-//     })
-
-
-var tagAPI = 'http://localhost:3000/Tag'
+var tagAPI = 'https://my-json-server.typicode.com/Akatsuki-Naruto/My-JSON-Server/Tag'
 
 function start() {
     getTags(function (Tag) {
@@ -43,33 +33,3 @@ function renderTags(Tag) {
     });
     listTagBlock.innerHTML = htmls.join('');
 }
-
-
-
-
-
-
-
-
-
-// fetch(tagAPI) 
-//     .then(response => response.json())
-//     .then(function(Tags){
-//         var htmls = Tags.map(function (Tag) {
-//             return `
-//             <button >
-//                 <div class="avatarTag"></div>
-//                 <div class="tittleTag">${Tag.title}</div>
-//                 <div class="timeTag"></div>
-//                 <div class="imageTag">${Tag.picture}</div>
-//             </button>`;
-//         });
-
-//         var html = htmls.join('');
-//         document.getElementById('listTags').innerHTML = html
-//     })
-//     .catch(function(err){
-//         alert('error')
-// })
-
-
